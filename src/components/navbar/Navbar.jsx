caf433bd7eeb3018b37875cd32ccee1154adc3d8
Sanjiv Chemudupati <sanjivch@users.noreply.github.com>
2024-09-08 17:39:27 +0000

    fixed navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,18 +8,9 @@ const Navbar = ({ menuOpenHome, setMenuOpenHome,  menuOpenPortfolio, setMenuOpen
                 
                 <div className="nav-items">
                 
-                    <a className={"nav-item " + (menuOpenHome && "active")} onClick={()=>{setMenuOpenHome(true); setMenuOpenAbout(false); setMenuOpenPortfolio(false); setMenuOpenCertificates(false)}} href="#intro">Home</a>
-                
-{/*                 
-                    <a className={"nav-item " + (menuOpenAbout && "active")} onClick={()=>{setMenuOpenHome(false); setMenuOpenAbout(true); setMenuOpenPortfolio(false); setMenuOpenCertificates(false)}} href="#about">About</a>
-                
-                 */}
-                    <a className={"nav-item " + (menuOpenPortfolio && "active")} onClick={()=>{setMenuOpenHome(false); setMenuOpenAbout(false); setMenuOpenPortfolio(true); setMenuOpenCertificates(false)}} href="#portfolio">Portfolio</a>
-                
-{/*                 
-                    <a className={"nav-item " + (menuOpenCertificates && "active")} onClick={()=>{setMenuOpenHome(false); setMenuOpenAbout(false); setMenuOpenPortfolio(false); setMenuOpenCertificates(true)}} href="#certifications">Certifications</a>
-                     */}
-                
+                    <a className={"nav-item " + (menuOpenHome && "active")} onClick={()=>{setMenuOpenHome(true); setMenuOpenPortfolio(false)}} href="#intro">Home</a>
+                    <a className={"nav-item " + (menuOpenPortfolio && "active")} onClick={()=>{setMenuOpenHome(false);  setMenuOpenPortfolio(true)}} href="#portfolio">Portfolio</a>
+
             
                 </div>
                     
